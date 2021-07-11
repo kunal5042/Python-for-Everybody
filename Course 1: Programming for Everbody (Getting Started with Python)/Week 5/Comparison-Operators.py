a1bb4539ad690995998eb0e21117ff41191bba4a
kunal5042 <againonlyimpstuff@gmail.com>
2021-07-11 22:37:30 +0530

    Comparison operators in Python.

---
+++ Course 1: Programming for Everbody (Getting Started with Python)/Week 5/Comparison-Operators.py
@@ -0,0 +1,22 @@
+variable1 = input("Enter the first number.\n")
+
+variable2 = input("Enter the second number.\n")
+
+
+if int(variable1) > int(variable2):
+    print("\nFirst number is greater than the second number.")
+          
+if int(variable1) < int(variable2):
+    print("\nFirst number is lesser than the second number.")
+          
+if int(variable1) == int(variable2):
+    print("\nFirst number is equal to the second number.")
+
+
+
+
+if int(variable1) != int(variable2):
+    print("Both numbers are different.")
+
+else:
+    print("Both numbers are same.")
