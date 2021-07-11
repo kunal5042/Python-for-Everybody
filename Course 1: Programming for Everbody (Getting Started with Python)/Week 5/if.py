3a6d618e2ece845342efb22719c85015c5a32e54
kunal5042 <againonlyimpstuff@gmail.com>
2021-07-11 22:37:48 +0530

    Conditional if in Python.

---
+++ Course 1: Programming for Everbody (Getting Started with Python)/Week 5/if.py
@@ -0,0 +1,12 @@
+name = input("Hi, what's your name?\n")
+
+print("\n")
+
+if name == "Kunal":
+    print("Hey, my name is Kunal too.\nNice to meet ya!")
+
+elif name == "Tanya":
+    print("Hi, Tanya\nI miss you <3")
+
+else:
+    print("Hi " + name + "! btw, I am Kunal.\nNice to meet ya!")
