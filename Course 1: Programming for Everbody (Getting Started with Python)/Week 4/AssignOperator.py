8bd3d00b769bbbb5a02b09a7b1386cb6fc088dce
kunal5042 <againonlyimpstuff@gmail.com>
2021-07-11 20:42:09 +0530

    Assignment operator in Python

---
+++ Course 1: Programming for Everbody (Getting Started with Python)/Week 4/AssignOperator.py
@@ -0,0 +1,35 @@
+print("Assignment operator example:")
+
+#Example:
+print("Hi!")
+
+myName= "Kunal"
+print("\nMy name is " + myName)
+
+myLastName = "Wadhwa"
+print("My last name is " + myLastName)
+
+myFullName = myName + " " + myLastName
+print("\nMy full name is " + myFullName)
+
+age= 19
+age= age + 1
+print(("I am ") +  str(age) + (" years old."))
+
+print("\nAnd, I am really excited about learning python programming.")
+
+
+
+
+
+#Output :
+#Assignment operator example:
+#Hi!
+#
+#My name is Kunal
+#My last name is Wadhwa
+#
+#My full name is Kunal Wadhwa
+#I am 20 years old.
+
+#And, I am really excited about learning python programming.
