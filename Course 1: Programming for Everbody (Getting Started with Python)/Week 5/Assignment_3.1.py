c61fcbcd821975a8c6c61aa1d9e1e58751b77dba
kunal5042 <againonlyimpstuff@gmail.com>
2021-07-11 22:39:19 +0530

    My answers to graded assignments of week 5.

---
+++ Course 1: Programming for Everbody (Getting Started with Python)/Week 5/Assignment_3.1.py
@@ -0,0 +1,34 @@
+"""
+3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking the user input - assume the user types numbers properly.
+"""
+
+#print("This program computes gross pay.\n")
+hours = input("Enter the number of hours:\n")
+try:
+    H = float(hours)
+except:
+    print("Not a valid number.")
+    print("Default hours value is 0 and will be used.")
+    H = 0
+
+hourlyRate = input("Enter the hourly rate:\n")
+try:
+    HR = float(hourlyRate)
+except:
+    print("Not a valid number.")
+    print("Default hourly rate value is 0 and will be used.")
+    HR = 0
+
+if H > 40:
+    extraHours = H - 40
+
+print( 40*HR + (extraHours*1.5*HR))
+
+"""
+Input:
+Hours = 45
+Rate  = 10.05
+
+Desired Output:
+498.75
+"""
---
+++ Course 1: Programming for Everbody (Getting Started with Python)/Week 5/Assignment_3.3.py
@@ -0,0 +1,44 @@
+"""
+3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+Score Grade
+>= 0.9 A
+>= 0.8 B
+>= 0.7 C
+>= 0.6 D
+< 0.6 F
+If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+"""
+
+score = input("Please enter a score between 0.0 and 1.0.\n")
+
+try:
+    S = float(score)
+except:
+    print("Not a valid number.")
+
+if S > 1.0 or S < 0:
+    print("The score is out of range.")
+else:
+    if S >= 0.9:
+        grade = "A"
+    elif S >= 0.8:
+        grade = "B"
+    elif S >= 0.7:
+        grade = "C"
+    elif S >= 0.6:
+        grade = "D"
+    else:
+        grade = "F"
+
+try:
+    print(grade)
+except:
+    print("Couldn't compute grade, because either the score wasn't valid or it was out of range.")
+
+
+"""
+Input: 0.85
+
+Desired Output:
+B
+"""
