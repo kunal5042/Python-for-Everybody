3536c4f90b1d1fa03d98dbee5c0ef6a0b28ebdb4
kunal5042 <againonlyimpstuff@gmail.com>
2021-07-11 17:26:00 +0530

    Spelling error in Reserved-Words.py

--- Course 1: Programming for Everbody (Getting Started with Python)/Week 4/Reserved-Words.py
+++ Course 1: Programming for Everbody (Getting Started with Python)/Week 4/Reserved-Words.py
@@ -1,3 +1,3 @@
 print("\nThe following words are are reserved in the python language as variable names /identifiers:\n")
 
-print(" False\t class\t return\t is\t finally\n None\t if\t for \t lambda\t continue\n True\t def\t from\t while\t nonlocal\n and\t del\t gloabl\t not\t with\n as\t elif\t try\t or\t yeild\n assert\t else\t import\t pass\t break\n except\t in\t raise\t")
+print(" False\t class\t return\t is\t finally\n None\t if\t for \t lambda\t continue\n True\t def\t from\t while\t nonlocal\n and\t del\t global\t not\t with\n as\t elif\t try\t or\t yeild\n assert\t else\t import\t pass\t break\n except\t in\t raise\t")
