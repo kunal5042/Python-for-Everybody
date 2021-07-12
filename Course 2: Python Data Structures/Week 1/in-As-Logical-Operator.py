0d9a7d6b4618de42760087556aa06eb83a372e8b
kunal5042 <againonlyimpstuff@gmail.com>
2021-07-12 16:04:05 +0530

    In as a logical operator in python

---
+++ Course 2: Python Data Structures/Week 1/in-As-Logical-Operator.py
@@ -0,0 +1,31 @@
+"""
+in keyword can be used as a logical opeartor in python.
+
+The in keyword can also be used to check to see if one string is "in" another string.
+
+The in expression is a logical expression that returns True or False and can be used in an if statment
+
+"""
+
+#Case sensitive.
+
+stringVar = "This is a string"
+
+print('String' in stringVar)
+
+if "Kunal" in stringVar:
+    exit()
+
+print("is" in stringVar)
+
+print("STRING" in stringVar)
+
+#Returns boolean value.
+       
+"""
+Output:
+False
+True
+False
+
+"""
