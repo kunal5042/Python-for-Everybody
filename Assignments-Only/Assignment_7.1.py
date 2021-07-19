50d363e599e2be11c26c758ed207fb3fbd8727d2
kunal5042 <againonlyimpstuff@gmail.com>
2021-07-19 14:58:14 +0530

    Modified some programs to use urllib instead of creating sockets

--- Assignments-Only/Assignment_7.1.py
+++ Assignments-Only/Assignment_7.1.py
@@ -3,8 +3,7 @@
 You can download the sample data at http://www.py4e.com/code3/words.txt
 """
 
-import os
-import socket
+import urllib.request, urllib.parse, urllib.error
 
 def getFileHandle():
     try:
@@ -12,60 +11,14 @@ def getFileHandle():
         file = open(fileName)
         return file
     except:
-            directory = os.getcwd()
             print("\nSample data file not found.")
             print("I'll download it for you!\n\nProgram execution will continue once the sample data is available")
-        
-        #Create a new socket to connect to the web
-            mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #Connect to the host data.pr4e.org on port 80
-            mySock.connect(('data.pr4e.org',80))
 
-        #GET command for the file we seek.
-            cmd = 'GET http://data.pr4e.org/words.txt HTTP/1.0\r\n\r\n'.encode()
-
-        #On successful connection with the server, send the request.
-            mySock.send(cmd)
-
-        #Get the current directory
-            directory = os.getcwd()
-        #New file's directory
-            filepath = directory + '/words.txt'
-
-        #Create a new text file to put the received data into.
-            file = open(filepath, 'w+')
-
-
-            flag = True
-        #While the received data's length greater than 1
-            while True:
-            #Keep receiving data
-                data = mySock.recv(1024)
+            fileHandle = urllib.request.urlopen('http://data.pr4e.org/words.txt')
             
-            #Break if length's less than 1
-                if (len(data)<1):
-                    break
-
-            #Runs only once
-                if flag:
-                    string = data.decode()
-                    index = string.find('Content-Type: text/plain')
-                    index = index + len('Content-Type: text/plain')
-                    string = string[index:]
-                    file.write(string)
-
-            #Runs only once, to take care of info received from the server which we don't need in our file.
-                if flag:
-                    flag = False
-                    continue
-
-                else:
-                #Runs till the loop is stopped
-                #Keep writing the data to the file
-                    file.write(data.decode())
-            print("Done\n\n")
-            file.seek(0)
-            return file
+            print("\nDESIRED OUTPUT:\n")
+            print((fileHandle.read()).decode().upper().strip())
+            quit()
 
 file = getFileHandle()
 
--- Assignments-Only/Assignment__10.5.py
+++ Assignments-Only/Assignment__10.5.py
@@ -20,7 +20,7 @@ Desired Output:
 19 1
 """
 import os
-import socket
+import urllib.request, urllib.parse, urllib.error
 
 #Creates a file handle and returns it.
 def getFileHandle():
@@ -29,61 +29,20 @@ def getFileHandle():
         file = open("mbox-short.txt")
         return file
     except:
-        directory = os.getcwd()
         print("\nSample data file not found.")
-        print("I'll download it for you!\n\nProgram execution will continue once the sample data is available")
-        
-        #Create a new socket to connect to the web
-        mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #Connect to the host data.pr4e.org on port 80
-        mySock.connect(('data.pr4e.org',80))
+        print("I'll download it for you!\n\nProgram execution will continue once the sample data is available\n\n")
+        fileHandle = urllib.request.urlopen('http://data.pr4e.org/mbox-short.txt')
 
-        #GET command for the file we seek.
-        cmd = 'GET http://data.pr4e.org/mbox-short.txt HTTP/1.0\r\n\r\n'.encode()
-
-        #On successful connection with the server, send the request.
-        mySock.send(cmd)
-
-        #Get the current directory
         directory = os.getcwd()
-        #New file's directory
-        filepath = directory + '/mbox-short.txt'
+        fileDir = directory + '/mbox-short.txt'
+        file = open(fileDir, 'w+')
 
-        #Create a new text file to put the received data into.
-        file = open(filepath, 'w+')
-
-
-        flag = True
-        #While the received data's length greater than 1
-        while True:
-
-            #Keep receiving data
-            data = mySock.recv(1024)
-            
-            #Break if length's less than 1
-            if (len(data)<1):
-                break
-
-            #Runs only once
-            if flag:
-                string = data.decode()
-                index = string.find('Content-Type: text/plain')
-                index = index + len('Content-Type: text/plain')
-                string = string[index:]
-                file.write(string)
-
-            #Runs only once, to take care of info received from the server which we don't need in our file.
-            if flag:
-                flag = False
-                continue
-
-            else:
-                #Runs till the loop is stopped
-                #Keep writing the data to the file
-                file.write(data.decode())
-        print("Done\n\n")
-        file.seek(0)
-        return file
+        for line in fileHandle:
+            #print(line.decode().strip())
+            file.write(line.decode())
+    file.seek(0)
+    print("\nDESIRED OUTPUT:\n")
+    return file
 
 
 #Creates a list of required lines for parsing.
